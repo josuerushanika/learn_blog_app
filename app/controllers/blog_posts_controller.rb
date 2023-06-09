7c0293665bbc907199f5c94ff86b8e3d39cea9ad
josuerushanika <josuerushanika@gmail.com>
2023-06-09 19:19:30 +0200

    Update action mail

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -51,5 +51,7 @@ class BlogPostsController < ApplicationController
 
     def set_blog_post
         blog_post = BlogPost.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            redirect_to root_path
     end
 end
